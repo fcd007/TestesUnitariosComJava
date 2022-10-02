26fa4642924ed784a784d6b62044d82a89e26d6b
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2022-10-02 16:24:42 -0300

    feat: adicionada classe calculadora

---
+++ src/main/java/br/ce/wcaquino/servicos/Calculadora.java
@@ -0,0 +1,22 @@
+package br.ce.wcaquino.servicos;
+
+import br.ce.wcaquino.entidades.exceptions.NaoPodeDividirPorZeroException;
+
+public class Calculadora {
+
+	public int somar(int numero1, int numero2) {
+		return numero1 + numero2;
+	}
+
+	public int sutrair(int numero1, int numero2) {
+		return numero1 - numero2;
+	}
+
+	public int dividir(int numero1, int numero2) throws NaoPodeDividirPorZeroException {
+		if(numero2 == 0 ) {
+			throw new NaoPodeDividirPorZeroException();
+		}
+		return numero1 / numero2;
+	}
+
+}
